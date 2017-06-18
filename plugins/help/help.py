4a3c7ca1f50ae7792ed1f5e4e487092a0babbefb
Brett Currie <theycallmevotum@gmail.com>
2017-06-18 18:27:06 -0300

    Add files via upload

--- plugins/help/help.py
+++ plugins/help/help.py
@@ -33,8 +33,20 @@ def create_help_embed(icon, log, author):
                  value="ed.joke — hear a hilarious joke."
                        "\ned.flip — flip a coin."
                        "\ned.range — choose a number between zero and x."
-                       "\ned.gif — search for a gif."
-                       "\ned.rpg — generate a random rpg character sheet.",
+                       "\ned.gif — search for a gif.",
+                 inline=True)
+
+    em.add_field(name='RPG2 Commands — ed.rpg2.<command>',
+                 value="rpg2 — basic command, if user has no character, create one for them, otherwise display users "
+                       "player data."
+                       "\nrpg2.fight — generate a random monster for the player to fight."
+                       "\nrpg2.potion —use one of your players potion if one is available and you aren't already full "
+                       "hp."
+                       "\nrpg2.freepotion — check if you have a free potion available, players may take one every 20 "
+                       "minutes."
+                       "\nrpg2.leaders — check the current leaderboards for all living characters."
+                       "\nrpg2.fallen — check the best players that have perished while in battle."
+                       "\nrpg2.duel @(x) — duel a specific player.",
                  inline=True)
 
     em.set_footer(text=str(datetime.now().strftime('%b %d, %Y %H:%M')),
